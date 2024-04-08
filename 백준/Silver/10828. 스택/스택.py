6e60f360040f5bbd6749968d44378e1b5bf16ac3
YunJuYoung <76280200+helloju817@users.noreply.github.com>
2024-04-08 15:17:41 +0900

    [Silver IV] Title: 스택, Time: 416 ms, Memory: 34188 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,42 @@
+N = int(input())
+stack = []
+oper = []
+
+def push(stack, item):
+    stack.append(int(item))
+
+def pop(stack):
+    if stack:
+        return stack.pop()
+    else:
+        return -1
+
+def size(stack):
+    return len(stack)
+
+def is_empty(stack):
+    if stack:  
+        return 0
+    else:          
+        return 1
+
+def top(stack):
+    if stack:
+        return stack[-1]
+    else:
+        return -1
+        
+for x in range(N):
+    oper.append(input().split())
+
+for x in oper:
+    if x[0] == 'push':
+        push(stack, x[1])
+    elif x[0] == 'pop':
+        print(pop(stack))
+    elif x[0] == 'size':
+        print(size(stack))
+    elif x[0] == 'empty':
+        print(is_empty(stack))
+    elif x[0] == 'top':
+        print(top(stack))
